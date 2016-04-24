a8cea33aa67502dd55abd6c3d6910ad8bef7487b
Acezio <bolide2005@163.com>
2016-04-24 20:01:36 +0800

    date:2016.04.24 autor:acezio.han message:flask jinja2 template practice

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from app import app
-
+from flask import render_template
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    user = {"name": "acezio", "nickname": "whatever"}
+    color_of_fruit = [
+        {"name": "apple", "color": "red"},
+        {"name": "banana", "color": "yellow"},
+        {"name": "watermelon", "color": "green"},
+    ]
+    return render_template("index.html",
+                           user=user,
+                           fruitlist=color_of_fruit)
