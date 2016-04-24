5aec6696ccbc2978625b8ae96b15bd5311af8994
Acezio <bolide2005@163.com>
2016-04-24 22:01:33 +0800

    date:2016.04.24 author:acezio.han message:login function demon

--- app/views.py
+++ app/views.py
@@ -1,14 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from forms import LoginForm
+
 
 @app.route("/")
+@app.route("/index", methods=['GET', 'POST'])
 def index():
-    user = {"name": "acezio", "nickname": "whatever"}
-    color_of_fruit = [
-        {"name": "apple", "color": "red"},
-        {"name": "banana", "color": "yellow"},
-        {"name": "watermelon", "color": "green"},
-    ]
-    return render_template("index.html",
-                           user=user,
-                           fruitlist=color_of_fruit)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("OpenID=" + form.openid.data + "and remember me is" + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template("forms.html", title="sign in", form=form)
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 from app import app
+from flask.ext.script import Manager
 
-app.run(debug=True)
+manager = Manager(app)
+if __name__ == "__main__":
+    #manager.run()
+    app.run(debug=True)
