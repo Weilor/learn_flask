b0b234d2105f7e5c8ef2088def7d07c4f2270bc2
Acezio <bolide2005@163.com>
2016-04-25 22:13:35 +0800

    date:2016.04.25 author:acezio.han message:db migration

--- app/models.py
+++ app/models.py
@@ -2,9 +2,22 @@ from app import db
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
+    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
     def __repr__(self):
         print(self.nickname)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        print(self.body)
