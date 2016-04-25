a2b22924edd7f6415f7cf6d0b44c2b493f545ba8
Acezio <bolide2005@163.com>
2016-04-25 21:48:28 +0800

    date:2016.04.25 author:acezio.han message:db operatoration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+
 
 app = Flask(__name__)
 app.config.from_object("config")
-from app import views
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app import views, models
 
 
--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import app
 from flask.ext.script import Manager
+from app import MigrateCommand
 
 manager = Manager(app)
 if __name__ == "__main__":
-    #manager.run()
-    app.run(debug=True)
+    manager.add_command('db', MigrateCommand)
+    manager.run()
+    #app.run(debug=True)
