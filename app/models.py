02e7bc76b7dbc42be3ad989b5d751e3ac5879468
Acezio <bolide2005@163.com>
2016-04-26 20:16:36 +0800

    date:2016.04.26 author:acezio.han message:user verified(part I)

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -6,7 +7,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
-    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError("password is write-only")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         print(self.nickname)
